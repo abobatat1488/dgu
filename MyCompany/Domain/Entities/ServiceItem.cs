b3454dd11370651dcd49aea13e13c6206f31fee1
Admin05 <Admin05@ADMIN05-PC>
2022-04-29 11:28:47 +0300

    Добавьте файлы проекта.

---
+++ MyCompany/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Заполните название страницы")]
+        [Display(Name = "Название страницы")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Краткое описание страницы")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание страницы")]
+        public override string Text { get; set; }
+    }
+}
